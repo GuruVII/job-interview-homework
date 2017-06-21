88c342f1df25b0530398bd9d37b98cc1e42eba67
GuruVII <guruvii@gmail.com>
2017-06-22 01:57:07 +0200

    linted the code

--- src/js/main.js
+++ src/js/main.js
@@ -1,82 +1,82 @@
 require('../scss/style.scss');
-let twoColumnsObject
-let otherColumnChildren
-const leftDiv = document.getElementById("left");
-const rightDiv = document.getElementById("right");
-const moveLeft = document.getElementById("left-button");
-const moveRight = document.getElementById("right-button");
-const className = document.getElementsByClassName("item");
-const selectedItems = document.getElementsByClassName("selected");
+let twoColumnsObject;
+let otherColumnChildren;
+const leftDiv = document.getElementById('left');
+const rightDiv = document.getElementById('right');
+const moveLeft = document.getElementById('left-button');
+const moveRight = document.getElementById('right-button');
+const className = document.getElementsByClassName('item');
+const selectedItems = document.getElementsByClassName('selected');
 
-//Object constructor that can accept an object of any size as an argument.
+//OBJECT CONSTRUCTOR that can accept an object of any size as an argument.
 function TwoColumns(items) {
     for (let key in items) {
-        this[key] = items[key]
+        this[key] = items[key];
     }
     this.GetItemPosition = (item) => {
         console.log(`The current item position of ${this[item].text} is ${this[item].position}`);
         return this[item].position;
-    }
+    };
     this.Save = () => {
         let save = JSON.stringify(this);
-        localStorage.setItem("save", save);
-        alert("State saved!")
-    }
+        localStorage.setItem('save', save);
+        alert('State saved!');
+    };
     this.Restore = () => {
-        localStorage.removeItem("save");
-        alert("Defaults restored")
+        localStorage.removeItem('save');
+        alert('Defaults restored');
         location.reload();
 
-    }
-};
+    };
+}
 
 
-//init function, which creates the object, sorts objects inside the object on their position, assigns them to the left or right column objects and inserts HTML code onto the page
+//INIT FUNCTION, which creates the object, sorts objects inside the object on their position, assigns them to the left or right column objects and inserts HTML code onto the page
 let widgetInit = () => {
-    if (localStorage.getItem("save") != undefined) {
-        twoColumnsObject = new TwoColumns(JSON.parse(localStorage.getItem("save")));
+    if (localStorage.getItem('save') != undefined) {
+        twoColumnsObject = new TwoColumns(JSON.parse(localStorage.getItem('save')));
     } else {
         twoColumnsObject = new TwoColumns({
             itemA: {
-                text: "Item A",
-                position: "left"
+                text: 'Item A',
+                position: 'left'
             },
             itemB: {
-                text: "Item B",
-                position: "left"
+                text: 'Item B',
+                position: 'left'
             },
             itemC: {
-                text: "Item C",
-                position: "left"
+                text: 'Item C',
+                position: 'left'
             },
             itemD: {
-                text: "Item D",
-                position: "right"
+                text: 'Item D',
+                position: 'right'
             },
             itemE: {
-                text: "Item E",
-                position: "right"
+                text: 'Item E',
+                position: 'right'
             }
         });
     }
     for (let key in twoColumnsObject) {
         //checks every property of the twoColumnsObject to see, if it is an object
-        if (typeof twoColumnsObject[key] == "object") {
-            if (twoColumnsObject[key]['position'] === "right") {
+        if (typeof twoColumnsObject[key] == 'object') {
+            if (twoColumnsObject[key]['position'] === 'right') {
                 rightDiv.innerHTML += `<div class="item" id ="${key}">${twoColumnsObject[key].text}</div>`;
 
             } else {
                 leftDiv.innerHTML += `<div class="item" id ="${key}">${twoColumnsObject[key].text}</div>`;
             }
         }
-    };
+    }
     select();
     //where it will move, id of current DIV and current position
-    moveElements(moveLeft, rightDiv, "right");
-    moveElements(moveRight, leftDiv, "left");
-    saveOrDefault("Save");
-    saveOrDefault("Restore");
-}
+    moveElements(moveLeft, rightDiv, 'right');
+    moveElements(moveRight, leftDiv, 'left');
+    saveOrDefault('Save');
+    saveOrDefault('Restore');
+};
 
 let select = () => {
     //adds click event listeners to every element with the class "item"
@@ -85,22 +85,22 @@ let select = () => {
         element.removeEventListener('click', selectFunction);
         element.addEventListener('click', selectFunction);
     });
-}
+};
 
 let selectFunction = (e) => {
-    let selectedDiv = document.getElementById(e.target.id)
+    let selectedDiv = document.getElementById(e.target.id);
     //removes "selected class from all elements in the other column"
-    if (e.target.parentNode.id === "right") {
+    if (e.target.parentNode.id === 'right') {
         otherColumnChildren = leftDiv.children;
     } else {
         otherColumnChildren = rightDiv.children;
-    };
+    }
     Array.from(otherColumnChildren).forEach(function(item) {
         document.getElementById(item.id).classList.remove('selected');
     });
-    //checks if ctrl key is beeing held    
+    //checks if ctrl key is beeing held
     if (e.ctrlKey) {
-        selectedDiv.classList.toggle('selected')
+        selectedDiv.classList.toggle('selected');
     } else {
         Array.from(className).forEach(function(element) {
             //this serves almost the same purpose as a classList.toggle, which cannot work in this case
@@ -110,9 +110,9 @@ let selectFunction = (e) => {
                 element.classList.remove('selected');
                 selectedDiv.classList.add('selected');
             }
-        })
+        });
     }
-}
+};
 
 
 
@@ -126,42 +126,42 @@ let moveElements = (button, newPosition, positionString) => {
     let errorCatching = () => {
         if (selectedItems[0] != undefined) {
             if (twoColumnsObject[selectedItems[0].id].position != positionString) {
-                return true
+                return true;
             } else {
-                return false
+                return false;
             }
         } else {
-            return true
+            return true;
         }
-    }
+    };
 
-    button.addEventListener('click', (e) => {
+    button.addEventListener('click', () => {
         //an addition to the errorCatching function, we also check if there is anything in selectedItems
         if (errorCatching() && selectedItems.length > 0) {
             Array.from(selectedItems).forEach(function(element) {
                 selected = document.getElementById(element.id);
                 selected.parentNode.removeChild(selected);
-                twoColumnsObject[element.id]["position"] = positionString;
-                newPosition.innerHTML += `<div class="item" id ="${element.id}">${twoColumnsObject[element.id].text}</div>`
+                twoColumnsObject[element.id]['position'] = positionString;
+                newPosition.innerHTML += `<div class="item" id ="${element.id}">${twoColumnsObject[element.id].text}</div>`;
                 twoColumnsObject.GetItemPosition(element.id);
                 select();
-            })
+            });
         } else {
-            let currentPosition
-            if (positionString == "right") {
-                currentPosition = "left";
+            let currentPosition;
+            if (positionString == 'right') {
+                currentPosition = 'left';
             } else {
-                currentPosition = "right";
+                currentPosition = 'right';
             }
             alert(`You have not selected any items in the ${currentPosition} column. 
-                Please select an item and try again`)
+                Please select an item and try again`);
         }
-    })
+    });
 
-}
+};
 
 let saveOrDefault = (action) => {
-    document.getElementById(action).addEventListener('click', twoColumnsObject[action])
-}
+    document.getElementById(action).addEventListener('click', twoColumnsObject[action]);
+};
 
 widgetInit();
