90772ed06c9f05bed416083ca471105c9f38964a
GuruVII <guruvii@gmail.com>
2017-06-21 03:24:00 +0200

    first commit

---
+++ README.md
@@ -0,0 +1,22 @@
+# Job-interview-homework
+A widget created for a job interview
+
+## Requirements
+
+  * [Node.js](http://nodejs.org)
+
+## Quickstart
+
+  * Clone the repository
+
+  * Install the Node modules required for development
+```bash
+npm install
+```
+  * Start Webpack
+```bash
+npm run webpack
+```
+
+### Basic template Source
+* https://github.com/ValentinGavan/es6-sass-webpack
---
+++ webpack.config.js
@@ -0,0 +1,40 @@
+var path = require('path');
+var webpack = require('webpack');
+var ExtractTextPlugin = require('extract-text-webpack-plugin');
+var LiveReloadPlugin = require('webpack-livereload-plugin');
+
+module.exports = {
+  entry: './src/js/main.js',
+  output: {
+    path: path.resolve(__dirname, 'public/'),
+    filename: 'js/[name].js'
+  },
+  watch: true,
+  module: {
+    loaders: [
+      {
+        test: /\.js$/,
+        loader: 'babel-loader',
+        query: {
+          presets: ['es2015']
+        },
+      },
+      {
+        test: /\.scss$/,
+        loader: ExtractTextPlugin.extract('css!sass')
+      }
+    ]
+  },
+  plugins: [
+      new ExtractTextPlugin('css/style.css', {
+          allChunks: true
+      }),
+      new LiveReloadPlugin({
+        appendScriptTag : true  
+      })
+  ],
+  stats: {
+    colors: true
+  },
+  // devtool: 'source-map'
+};
